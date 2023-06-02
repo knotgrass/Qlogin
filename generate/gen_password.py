2e1173c142e8cf3aea626765283c6433f5199cfb
vietanh112 <imnova1212@gmail.com>
2023-06-02 15:37:01 +0700

    refactor

--- generate/gen_password.py
+++ generate/gen_password.py
@@ -1,4 +1,8 @@
-from random import choice, randint, shuffle, seed as seed_
+from random  import (choice  as choice_,
+                     randint as randint_,
+                     shuffle as shuffle_,
+                     seed    as seed_ )
+
 from .character import Ambiguous, Unambiguous
 
 
@@ -13,17 +17,17 @@ def get_random_passwd(size:int=15, min_num:int=1, min_punc:int=1,
 
     # 8 = num_of_letters include both lower & upper
     min_num_of_letters = size // 2 + 1
-    num_of_num = randint(min_num, size - min_punc - min_num_of_letters)
-    num_of_punc = randint(min_punc, size - num_of_num - min_num_of_letters)
+    num_of_num = randint_(min_num, size - min_punc - min_num_of_letters)
+    num_of_punc = randint_(min_punc, size - num_of_num - min_num_of_letters)
     num_of_lower = size - num_of_num - num_of_punc - 1
     num_of_upper = size - num_of_num - num_of_punc - num_of_lower
 
-    passwd = [choice(character.lower) for _ in range(num_of_lower)]    \
-           + [choice(character.upper) for _ in range(num_of_upper)]     \
-           + [choice(character.punctuation) for _ in range(num_of_punc)] \
-           + [choice(character.number) for _ in range(num_of_num)]
+    passwd = [choice_(character.lower) for _ in range(num_of_lower)]    \
+           + [choice_(character.upper) for _ in range(num_of_upper)]     \
+           + [choice_(character.punctuation) for _ in range(num_of_punc)] \
+           + [choice_(character.number) for _ in range(num_of_num)]
 
-    shuffle(passwd)
+    shuffle_(passwd)
     str_passwd = ''.join(passwd)
     # print(str_passwd)
     # print(f'num_of_num = {num_of_num}\nnum_of_punc = {num_of_punc} \nnum_of_lower = {num_of_lower} \nnum_of_upper = {num_of_upper}')
