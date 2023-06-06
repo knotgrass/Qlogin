dee0b03fcadc63bc296b6f741adc2f7b3a23fbf3
vietanh112 <imnova1212@gmail.com>
2023-06-06 13:52:34 +0700

    update download and url ref

--- generate/create_words.py
+++ generate/create_words.py
@@ -1,6 +1,8 @@
+from nltk import download
 from nltk.corpus import wordnet
+download('wordnet')
 
-
+# https://chat.openai.com/share/79c4993d-8060-4a74-acd1-acb81ce27c14
 def get_english_word(pos):
     words = []
     for synset in wordnet.all_synsets(pos=pos):
@@ -21,5 +23,5 @@ for pos in (wordnet.ADJ, wordnet.ADJ_SAT, wordnet.ADV, wordnet.NOUN, wordnet.VER
 words = set(words)
 with open('generate/eng.txt', 'w', encoding="ascii") as f:
     f.write('\n'.join(words))
-    
+
 print(len(words))
