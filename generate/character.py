9d6bac5bab196844f5e43192b49373f5955a6468
knotgrass <40136779+knotgrass@users.noreply.github.com>
2024-09-30 15:19:56 +0700

    https://cybersecuritynews.com/nist-rules-password-security/

--- generate/character.py
+++ generate/character.py
@@ -1,3 +1,4 @@
+import code
 from dataclasses import dataclass
 from string import (ascii_lowercase as lowercase_,
                     ascii_uppercase as uppercase_,
@@ -22,3 +23,13 @@ class Unambiguous(Character):
     upper:str = uppercase_.replace('I', '').replace('O', '') # "IO" capital i, capital o
     number:str = digits_[2:]                                 # "01" zero and one
     punctuation:str = punctuation_.replace('|', '')          # "|"  vertical bar
+
+@dataclass
+class Unicode(Character):
+    code_points:frozenset[int] = frozenset(range(0x110000))
+    printable_unicode_chars:frozenset[str] = frozenset(
+        chr(code_point) for code_point in code_points if chr(code_point).isprintable()
+    )
+    unprintable_unicode_chars:frozenset[str] = frozenset(
+        chr(code_point) for code_point in code_points if not chr(code_point).isprintable()
+    )
